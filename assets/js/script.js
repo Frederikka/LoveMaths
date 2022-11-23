e5b9a1cb4fbb4d642ea3b6b22aa4f6ee8a1fa20c
Frederikka <frances.boyle.work@gmail.com>
2022-11-23 17:10:33 +0000

    Add final changes for user and division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,7 +82,8 @@ if (operator === "+") {
 } else if (operator ==="-") { 
     return [operand1 - operand2, "subtract"]; 
 } else if (operator ==="/") { 
-    return [operand1 / operand2, "division"]; 
+    return [operand1 / operand2, "division"]
+    // return [Math.floor(operand1 / operand2), "division"]; 
 } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;  
@@ -132,8 +133,14 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 function  displayDivisionQuestion(operand1, operand2 ) {
 
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
-    document.getElementById('operator').textContent = "/";
+    operand1 = operand1 * operand2;
+    
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
+
+    // document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    // document.getElementById("operand2").textContent = operand1 * operand2 ? operand2 : operand1;
+    // document.getElementById('operator').textContent = "/";
 }
 
